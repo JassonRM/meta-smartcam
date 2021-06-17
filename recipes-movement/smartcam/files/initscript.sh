4a465824527d65c362758ca832eb603a045f1c59
Jasson Rodriguez <jassonrm@icloud.com>
2021-06-17 17:56:33 -0600

    Use original pipeline

--- recipes-movement/smartcam/files/initscript.sh
+++ recipes-movement/smartcam/files/initscript.sh
@@ -7,7 +7,7 @@ cd /webserver
 python3 ./server.py &
 
 logger "Starting video capture"
-gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg ! jpegdec ! tee name=split ! queue ! x264enc speed-preset=ultrafast ! 'video/x-h264,stream-format=byte-stream,profile=baseline' ! mpegtsmux ! hlssink max-files=5 target-duration=1 playlist-location=/var/www/localhost/html/stream.m3u8 location=/var/www/localhost/html/segment%6d.ts split. !  queue ! rtpvrawpay ! udpsink host=127.0.0.1 port=5000 &
+gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg ! tee name=split ! queue ! jpegdec ! x264enc speed-preset=ultrafast ! 'video/x-h264,stream-format=byte-stream,profile=baseline' ! mpegtsmux ! hlssink max-files=5 target-duration=1 playlist-location=/var/www/localhost/html/stream.m3u8 location=/var/www/localhost/html/segment%6d.ts split. ! queue ! rtpjpegpay ! udpsink host=127.0.0.1 port=5000 &
 
 detection &
 
